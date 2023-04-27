2bcf327ec8d97febff7d18bd26399e592bb5e828
JorgeLpzGnz <geolg763@gmail.com>
2023-04-27 14:28:57 -0500

    248. usePokemon hook

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react'
-import { View, Text } from 'react-native';
 import { Navigator } from './src/navigator/Navigator';
 import { NavigationContainer } from '@react-navigation/native';
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
-  return (
-    <View>
-        <Text>HomeScreen</Text>
-    </View>
-  )
+
+    const { top } = useSafeAreaInsets()
+
+    const { simplePokemonList } = usePokemonPaginated()
+
+    return (
+        <>
+
+            <Image
+                source={require('../assets/pokebola.png')}
+                style={styles.pokebolaBG}
+            />
+
+
+            <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20
+            }}>Pokedex</Text>
+        </>
+    )
 }
