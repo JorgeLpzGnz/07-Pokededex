d0038b8526ef96394883f7cd646d1dbcc70cf5d3
JorgeLpzGnz <geolg763@gmail.com>
2023-04-28 08:51:58 -0500

    256. Pantalla del pokemon

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigator/Navigator';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 
 // @param RootStackParams es el tipo de props que son
 // @param 'PokemonScreen' la pantalla en la que me encuentro
@@ -10,13 +13,91 @@ interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> { }
 
 export const PokemonScreen = ( { navigation, route }: Props) => {
 
-    const { simplePokemon } = route.params
+    const { simplePokemon, color } = route.params
+
+    const { name, id, picture } = simplePokemon
+
+    const { top } = useSafeAreaInsets()
 
     
     return (
         <View>
-            <Text>{ simplePokemon.name }</Text>
+            {/* Header Container */}
+            <View
+                style={{
+                    ...styles.headerContainer,
+                    backgroundColor: color,
+                }}
+            >
+                <TouchableOpacity
+                    onPress={() => navigation.pop()}
+                    activeOpacity={ 0.8 }
+                    style={{
+                        ...styles.backButton,
+                        top: top + 5,
+                    }}
+                >
+                    <Icon
+                        name="arrow-back-outline"
+                        color="white"
+                        size={ 35 }
+                    />
+                </TouchableOpacity>
+
+                {/* Nombre del pokemon */}
+                <Text 
+                    style={{ 
+                        ...styles.pokemonName,
+                        top: top + 40
+                    }}
+                >
+                    { name + '\n' }#{ id }
+                </Text>
+
+                {/* Pokebola Blanca */}
+                <Image
+                    source={ require('../assets/pokebola-blanca.png')}
+                    style={ styles.pokeball }
+                />
+
+                <FadeInImage
+                    uri={ picture }
+                    style={ styles.pokemonImage }
+                />
+            </View>
         </View>
     )
     
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 370,
+        zIndex: 999,
+        alignItems: 'center',
+        borderBottomRightRadius: 1000,
+        borderBottomLeftRadius: 1000,
+    },
+    backButton: {
+        position: 'absolute',
+        left: 20,
+    },
+    pokemonName: {
+        color: 'white',
+        fontSize: 40,
+        alignSelf: 'flex-start',
+        left: 20,
+    },
+    pokeball: {
+        width: 250,
+        height: 250,
+        bottom: -20,
+        opacity: 0.7,
+    },
+    pokemonImage: {
+        width: 250,
+        height: 250,
+        position: 'absolute',
+        bottom: -15,
+    }
+});
